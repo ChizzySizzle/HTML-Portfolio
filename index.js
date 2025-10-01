c1ddeb254d684d91108fb32f5cc867009b449422
ChizzySizzle <gabrielheiser8803@gmail.com>
2025-10-01 17:58:58 -0400

    Small changes
    Updated contact info page
    Made images clickable on large screens
    Slight style changes

--- index.js
+++ index.js
@@ -57,3 +57,16 @@ images.forEach(img => {
     });
   });
 });
+
+
+
+function getPics() {} //just for this demo
+const imgs = document.querySelectorAll('.thumbnail-container img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
